a3d60db36207dab930867417577940fe34b2ce6b
Dyikot <dimak.13dimak@gmail.com>
2024-07-22 19:01:28 +0500

    Initial commit

---
+++ Random.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <random>
+#include <ranges>
+#include <concepts>
+
+template<typename T>
+concept Numeric = std::convertible_to<T, int>;
+
+template<typename T>
+concept NumericRange = std::ranges::range<T> && Numeric<std::ranges::range_value_t<T>>;
+
+class Random
+{
+private:
+	std::random_device _device;
+	std::default_random_engine _engine;
+	std::uniform_int_distribution<> _distribution;
+public:
+	Random();
+
+	int NextInt(const int min, const int max);
+
+	template<Numeric Numeric>
+	std::vector<Numeric> GenerateVector(const int min, const int max, const size_t size);
+
+	template<NumericRange NumericRange>
+	void FillRange(NumericRange& range, const int min, const int max);
+};
+
+Random::Random()
+{
+	_engine.seed(_device());
+}
+
+int Random::NextInt(const int min, const int max)
+{
+	_distribution.param(std::uniform_int<>::param_type(min, max));
+	return _distribution(_engine);
+}
+
+template<Numeric Numeric>
+std::vector<Numeric> Random::GenerateVector(const int min, const int max, const size_t size)
+{
+	std::vector<Numeric> range(size);
+
+	for(auto& item : range)
+	{
+		item = NextInt(min, max);
+	}
+
+	return range;
+}
+
+template<NumericRange NumericRange>
+void Random::FillRange(NumericRange& range, const int min, const int max)
+{
+	for(auto& item : range)
+	{
+		item = NextInt(min, max);
+	}
+}
