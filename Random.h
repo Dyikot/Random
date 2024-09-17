d9a10fa5ef0d5774c19e788ed3abbd65c526df47
Dyikot <dimak.13dimak@gmail.com>
2024-09-17 12:58:50 +0500

    Added new methods: Next, NextDouble
    Also added new constructor in which you can specify the seed value.

--- Random.h
+++ Random.h
@@ -15,11 +15,28 @@ class Random
 private:
 	std::random_device _device;
 	std::default_random_engine _engine;
-	std::uniform_int_distribution<> _distribution;
+	std::uniform_int_distribution<> _intDistribution;
+	std::uniform_real_distribution<> _realDistribution;
 public:
 	Random() noexcept
 	{
 		_engine.seed(_device());
+	}
+
+	Random(uint32_t seed) noexcept
+	{
+		_engine.seed(seed);
+	}
+
+	/// <summary>
+	/// Generate a random uint32 number in a range [0, max]
+	/// </summary>
+	/// <param name="max"> - maximum value</param>
+	/// <returns>a random uint32 number in a range [0, max]</returns>
+	uint32_t Next(uint32_t max) noexcept
+	{
+		_intDistribution.param(std::uniform_int_distribution<>::param_type(0, max));
+		return _intDistribution(_engine);
 	}
 
 	/// <summary>
@@ -28,12 +45,32 @@ public:
 	/// <param name="min"> - minimal value</param>
 	/// <param name="max"> - maximum value</param>
 	/// <returns>a random number in a range [min, max]</returns>
-	int NextInt(const int min, const int max) noexcept
+	int NextInt(int min, int max) noexcept
 	{
-		_distribution.param(std::uniform_int_distribution<>::param_type(min, max));
-		return _distribution(_engine);
+		_intDistribution.param(std::uniform_int_distribution<>::param_type(min, max));
+		return _intDistribution(_engine);
 	}
 
+	/// <summary>
+	/// Generate a random real number in a range [min, max]
+	/// </summary>
+	/// <param name="min"> - minimal value</param>
+	/// <param name="max"> - maximum value</param>
+	/// <returns>a random real number in a range [min, max]</returns>
+	double NextDouble(double min, double max) noexcept
+	{
+		_realDistribution.param(std::uniform_real_distribution<>::param_type(min, max));
+		return _realDistribution(_engine);
+	}
+
+	/// <summary>
+	/// Generate a random real number in a range [0, 1]
+	/// </summary>
+	/// <returns>a random real number in a range [0, 1]</returns>
+	double NextDouble() noexcept
+	{
+		NextDouble(0, 1);
+	}
 	
 	/// <summary>
 	/// Generate std::vector&lt;Numeric&gt; in a range [min, max]
@@ -43,7 +80,7 @@ public:
 	/// <param name="size"> - length of vector</param>
 	/// <returns>a vector with random numbers in a range [min, max]</returns>
 	template<Numeric Numeric>
-	std::vector<Numeric> GenerateVector(const int min, const int max, const size_t size) noexcept
+	std::vector<Numeric> GenerateVector(int min, int max, size_t size) noexcept
 	{
 		std::vector<Numeric> range(size);
 
@@ -62,7 +99,7 @@ public:
 	/// <param name="min"> - minimal value</param>
 	/// <param name="max"> - maximum value</param>
 	template<NumericRange NumericRange>
-	void FillRange(NumericRange& range, const int min, const int max) noexcept
+	void FillRange(NumericRange& range, int min, int max) noexcept
 	{
 		for(auto& item : range)
 		{
